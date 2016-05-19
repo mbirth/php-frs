783891ce50e1e9321816a1ef3db1bbed55ce63b4
Markus Birth <mbirth@gmail.com>
2016-05-19 23:51:08 +0200

    Initial import

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/vendor' . '/autoload.php';
+
+$m = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates'),
+    'charset' => 'utf-8',
+    'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
+));
+
+$tpl = $m->loadTemplate('index_html');
+echo $tpl->render();
