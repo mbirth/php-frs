92f2408deb674e2dcbeea68ff414818ec8daa99f
Markus Birth <mbirth@gmail.com>
2016-05-20 13:19:03 +0200

    Moved mustache partials to separate directory.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor' . '/autoload.php';
 
 $m = new Mustache_Engine(array(
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates/partials'),
     'charset' => 'utf-8',
     'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
 ));
