2983bcb56b2369fc0e53c574485d2ea55acf3753
Markus Birth <mbirth@gmail.com>
2016-05-24 02:39:23 +0200

    Added templates for different INPUT fields. prep_form needs cleanup!

--- prep_form.php
+++ prep_form.php
@@ -8,10 +8,14 @@ $field_data_json = file_get_contents('definitions/' . $action . '.json');
 $field_data = json_decode($field_data_json, true);
 
 foreach ($field_data['groups'] as $id=>$group) {
-    $by_group[$group] = array();
+    $by_group[$group] = array(
+        'group_name' => $group,
+        'fields' => array(),
+    );
 }
 
 foreach ($field_data['fields'] as $key=>$meta) {
+    $meta['field_id'] = $key;
     $group_name = $field_data['groups'][$meta['group']];
     $meta['group_name'] = $group_name;
     if (isset($_SESSION[$skey][$key])) {
@@ -19,16 +23,27 @@ foreach ($field_data['fields'] as $key=>$meta) {
     } elseif (isset($meta['default'])) {
         switch ($meta['default']) {
             case 'USER_NAME':
-                $meta['value'] = 'User Name';
+                $meta['value'] = $data['user']['name_first'] . ' ' . $data['user']['name_last'];
                 break;
             case 'USER_EMAIL':
-                $meta['value'] = 'user@email.com';
+                $meta['value'] = $data['user']['email'];
                 break;
             default:
                 $meta['value'] = $meta['default'];
                 break;
         }
     }
-    $by_group[$group_name][$key] = $meta;
+    $meta['fieldtype_' . $meta['type']] = true;
+    if ($meta['type'] == 'datetime') {
+        $meta['min'] = date('Y-m-d');
+    }
+    $by_group[$group_name]['fields'][$key] = $meta;
 }
-print_r($by_group);
+
+foreach ($by_group as $group=>$group_data) {
+    $by_group[$group]['fields'] = array_values($group_data['fields']);
+}
+
+#print_r($by_group);
+$by_group = array_values($by_group);
+$data['form_data'] = $by_group;
