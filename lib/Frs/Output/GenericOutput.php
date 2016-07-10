0d57aace4b0489e930513dee95574421497ee225
Markus Birth <mbirth@gmail.com>
2016-07-10 16:24:03 +0200

    Fix naming of variables.

--- lib/Frs/Output/GenericOutput.php
+++ lib/Frs/Output/GenericOutput.php
@@ -16,13 +16,13 @@ class GenericOutput
      * @param string $templatesPath Path to templates. Must be a folder, no slash at end!
      * @param string $partialsPath Path to partials (relative to $templatesPath). Must be a folder, no slash at end!
      */
-    public function __construct($templatesPrefix = 'templates', $partialsPrefix = 'partials')
+    public function __construct($templatesPath = 'templates', $partialsPath = 'partials')
     {
-        $this->templatesPrefix = $templatesPrefix;
-        $this->partialsPrefix  = $templatesPrefix . DIRECTORY_SEPARATOR . $partialsPrefix;
+        $this->templatesPath  = $templatesPath;
+        $this->partialsPath   = $templatesPath . DIRECTORY_SEPARATOR . $partialsPath;
         $this->templateEngine = new \Mustache_Engine(array(
-            'loader' => new \Mustache_Loader_FilesystemLoader($this->templatesPrefix),
-            'partials_loader' => new \Mustache_Loader_FilesystemLoader($this->partialsPrefix),
+            'loader' => new \Mustache_Loader_FilesystemLoader($this->templatesPath),
+            'partials_loader' => new \Mustache_Loader_FilesystemLoader($this->partialsPath),
             'charset' => 'utf-8',
             'logger' => new \Mustache_Logger_StreamLogger('php://stderr'),
         ));
