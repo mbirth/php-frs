608854b57d9695d853828e4da6b7c2133958f05b
Markus Birth <mbirth@gmail.com>
2017-02-04 02:40:37 +0100

    Removed most Google Account management and mailing stuff.

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ use \Frs\Output\Transport\GScriptTransport;
 
 $stdout = new StdoutTransport();
 $ho = new HtmlOutput($stdout, dirname(__FILE__) . '/templates');
-$ho->setTemplateVar('session_time_left', 0);
 
 $sm = new SessionManager();
 
@@ -21,115 +20,75 @@ if (isset($_GET['action'])) {
 }
 $ho->setTemplateVar('action', $action);
 $ho->setTemplateVar('action_uc', ucwords($action));
+$ho->setTemplateVar('date_today', date('Y-m-d'));
 
-if (isset($_GET['code']) && $_GET['code']) {
-    $sm->authAndRedirect($_GET['code']);  // exits
-}
-
-$tpl_done = false;
-
-// route pages that work with and without login
 switch ($action) {
-    case 'logout':
-        $sm->logoutAndRedirect();  // exits
     case 'faq':
         $ho->setTemplate('faq_html');
-        $tpl_done = true;
         break;
+
     case 'send':
-        // Store input in session, in case the token timed out
+        // Store input in session, in case something happens
         $sm->storeFormData($_POST['form_type']);
-        break;
-}
+        echo 'This would send the mail...';
+        //$mt = new MailTransport($sm);
+        $mt = new GScriptTransport();
+        $mo = new MailOutput($mt, dirname(__FILE__) . '/templates');
+        $form_type = $_REQUEST['form_type'];
+        $mo->setTemplate('mail_' . $form_type);
+        $skey = 'form_' . $form_type;
+        $fd = new FieldDefinition($form_type);
+        $fd->setFieldValues($_SESSION[$skey]);
+        $fieldData = $fd->getFieldData();
+        $fields = $fieldData['fields'];
 
-if (!$tpl_done && $sm->hasSessionToken()) {
-    // Authenticated
-    $created = $_SESSION['access_token']['created'];
-    $expires = $_SESSION['access_token']['expires_in'];
-    $expire_stamp = intval($created) + intval($expires);
-    $ho->setTemplateVar('session_created', $created);
-    $ho->setTemplateVar('session_expires', $expires);
-    $ho->setTemplateVar('session_time_left', ($expire_stamp) - time());
-
-    try {
-        $sm->verifySession();
-    } catch (Exception $e) {
-        echo $e->getMessage();
-        #session_destroy();
-        die();
-    }
-
-    $userInfo = $sm->getUserinfo();
-    $ho->setTemplateVar('user', $userInfo);
-    $ho->setTemplateVar('date_today', date('Y-m-d'));
-
-    // Check $userdata->verifiedEmail and deny if not verified.
-    if (!$userInfo['verifiedEmail']) {
-        $ho->setTemplate('notverified_html');
-        $tpl_done = true;
-    } else {
-        switch ($action) {
-            case 'send':
-                echo 'This would send the mail...';
-                //$mt = new MailTransport($sm);
-                $mt = new GScriptTransport();
-                $mo = new MailOutput($mt, dirname(__FILE__) . '/templates');
-                $form_type = $_REQUEST['form_type'];
-                $mo->setTemplate('mail_' . $form_type);
-                $skey = 'form_' . $form_type;
-                $fd = new FieldDefinition($form_type);
-                $fd->setFieldValues($_SESSION[$skey]);
-                $fieldData = $fd->getFieldData();
-                $fields = $fieldData['fields'];
-
-                $data = $ho->getTemplateVars();
-                $data['email_date'] = date('r');
-                $data = array_merge($data, $fields);
-                $mo->setTemplateVars($data);
-                $mo->setTemplateVar('form_type', $form_type);
-                $mo->setTemplateVar('form_type_uc', ucwords($form_type));
-                $mo->setSubject('[FRS] ' . ucwords($form_type) . ' Reservation');
-                $mo->addRecipient($data['user']['email'], $data['user']['name_first'] . ' ' . $data['user']['name_last']);
-                $mail_sent = $mo->send();
-                if ($mail_sent) {
-                    $ho->setTemplate('mail_sent_html');
-                } else {
-                    $ho->setTemplate('mail_failed_html');
-                }
-                break;
-            default:
-                if (in_array($action, array('event', 'flight', 'hotel', 'restaurant', 'rentalcar', 'train', 'bus'))) {
-                    $ho->setTemplate($action . '_html');
-                    $tpl_done = true;
-                    $skey = 'form_' . $action;
-
-                    $placeholders = array(
-                        'USER_NAME'  => $userInfo['name_first'] . ' ' . $userInfo['name_last'],
-                        'USER_EMAIL' => $userInfo['email'],
-                    );
-                    $fd = new FieldDefinition($action);
-                    $fd->setPlaceholders($placeholders);
-                    if (isset($_SESSION[$skey])) {
-                        $fd->setFieldValues($_SESSION[$skey]);
-                    }
-
-                    $by_group = $fd->getGroups();
-
-                    // Convert hash to list for Mustache compatibility
-                    $by_group = array_values($by_group);
-                    $ho->setTemplateVar('form_data', $by_group);
-                }
-                if (!$tpl_done) {
-                    $ho->setTemplate('loggedin_html');
-                    $tpl_done = true;
-                }
-                break;
+        $data = $ho->getTemplateVars();
+        $data['email_date'] = date('r');
+        $data = array_merge($data, $fields);
+        $mo->setTemplateVars($data);
+        $mo->setTemplateVar('form_type', $form_type);
+        $mo->setTemplateVar('form_type_uc', ucwords($form_type));
+        $mo->setSubject('[FRS] ' . ucwords($form_type) . ' Reservation');
+        $mo->addRecipient($data['user']['email'], $data['user']['name_first'] . ' ' . $data['user']['name_last']);
+        $mail_sent = $mo->send();
+        if ($mail_sent) {
+            $ho->setTemplate('mail_sent_html');
+        } else {
+            $ho->setTemplate('mail_failed_html');
         }
-    }
-} elseif (!$tpl_done) {
-    // Not authenticated
-    $ho->setTemplateVar('auth_url', $sm->getAuthUrl());
-    $ho->setTemplate('index_html');
+        break;
+
+    case 'event':
+    case 'flight':
+    case 'hotel':
+    case 'restaurant':
+    case 'rentalcar':
+    case 'train':
+    case 'bus':
+        $ho->setTemplate($action . '_html');
+        $skey = 'form_' . $action;
+
+        $placeholders = array(
+            'USER_NAME'  => '',
+            'USER_EMAIL' => '',
+        );
+        $fd = new FieldDefinition($action);
+        $fd->setPlaceholders($placeholders);
+        if (isset($_SESSION[$skey])) {
+            $fd->setFieldValues($_SESSION[$skey]);
+        }
+
+        $by_group = $fd->getGroups();
+
+        // Convert hash to list for Mustache compatibility
+        $by_group = array_values($by_group);
+        $ho->setTemplateVar('form_data', $by_group);
+        break;
+
+    default:
+        // Show welcome page
+        $ho->setTemplate('welcome_html');
+        break;
 }
 
 $ho->send();
--- lib/Frs/SessionManager.php
+++ lib/Frs/SessionManager.php
@@ -4,97 +4,14 @@ namespace Frs;
 
 class SessionManager
 {
-    private $client;
-    private $googleAuthValid;
-
     public function __construct()
     {
-        $this->googleAuthValid = false;
-        $this->client = new \Google_Client();
-        $this->client->setApplicationName('Fake Reservation System');
-        $this->client->setAuthConfigFile('client_secret.json');
-        $this->client->addScope(\Google_Service_Oauth2::USERINFO_EMAIL);
-        $this->client->addScope(\Google_Service_Gmail::GMAIL_SEND);
-
         session_start();
-    }
-
-    public function getAuthUrl()
-    {
-        return $this->client->createAuthUrl();
-    }
-
-    public function authAndRedirect($authCode)
-    {
-        // Validate OAuth2 result, set access token and redirect to self
-        $this->client->authenticate($authCode);
-        $_SESSION['access_token'] = $this->client->getAccessToken();
-        $this->redirectAndExit();
-    }
-
-    public function logoutAndRedirect()
-    {
-        // Delete session and redirect to self
-        #$this->client->setAccessToken($_SESSION['access_token']);
-        #$this->client->revokeToken();   // removed granted permissions from account
-        $_SESSION = array();
-        if (ini_get('session.use_cookies')) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time()-42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-        }
-        session_destroy();
-        $this->redirectAndExit();
-    }
-
-    private function redirectAndExit()
-    {
-        header('Location: ' . $this->client->getRedirectUri());
-        exit(0);
     }
 
     public function storeFormData($form_type)
     {
         $skey = 'form_' . $form_type;
         $_SESSION[$skey] = $_POST;
-    }
-
-    public function hasSessionToken()
-    {
-        return (isset($_SESSION['access_token']) && $_SESSION['access_token']);
-    }
-
-    public function verifySession()
-    {
-        $this->client->setAccessToken($_SESSION['access_token']);
-        if ($this->client->isAccessTokenExpired()) {
-            throw new \Exception('Token expired. <a href="' . $this->getAuthUrl() . '">Request new one</a>.');
-        }
-
-        $this->googleAuthValid = true;
-    }
-
-    public function getUserinfo()
-    {
-        if (!$this->googleAuthValid) {
-            return array();
-        }
-        $oauth = new \Google_Service_Oauth2($this->client);
-        $userdata = $oauth->userinfo->get();
-
-        $result = array(
-            'name_first' => $userdata->givenName,
-            'name_last'  => $userdata->familyName,
-            'name'       => $userdata->name,
-            'picture'    => $userdata->picture,
-            'email'      => $userdata->email,
-            'gender'     => $userdata->gender,
-            'verifiedEmail' => $userdata->verifiedEmail,
-        );
-        return $result;
-    }
-
-    public function getGoogleClient()
-    {
-        return $this->client;
     }
 }
